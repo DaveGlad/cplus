cc2daa24f60f9e419b78f9cf138b735abd40f7c6
dave_glad <dave@dba.ma>
2021-12-20 00:07:46 +0100

    add form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import Radio from '../radio'
 import s from './assets/form.module.css'
@@ -5,6 +6,9 @@ import s from './assets/form.module.css'
 const Form = () => {
   return (
     <form action="" className={s.form}>
+      <h2 className={s.form__title}>VOS BESOINS</h2>
+      <Image src="/images/Frame 620.svg" width={250} height={20} />
+      <h2 className={s.form__title}>Quel est le niveau de remboursement souhaité ?</h2>
       <div className={s.content}>
         <h3> 🏥 SOINS Médicaux</h3>
         <Radio />
