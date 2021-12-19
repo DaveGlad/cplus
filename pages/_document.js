d78ef9cd92c0fed54e4770de30ea7a111bb1948f
dave_glad <dave@dba.ma>
2021-12-19 22:15:41 +0100

    fix display font

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class Cplus extends Document {
   render() {
     return (
       <Html lang="fr">
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
